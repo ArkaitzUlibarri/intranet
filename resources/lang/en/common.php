c78cf8070c18bac59ab5073852502d08ea55493d
ArkaitzUlibarri <arkaitz.ulibarri@hotmail.com>
2020-12-13 14:00:00 +0100

    Translations

---
+++ resources/lang/en/common.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'yes' => 'Yes',
+    'no' => 'No',
+
+    'users' => 'Users',
+    'new_user' => 'New User',
+    'home' => 'Home',
+
+    'id' => 'ID',
+
+    'name' => 'Name',
+    'email' => 'Email',
+    'email_verified_at' => 'Email Verified At',
+    'last_login' => 'Last Login',
+
+    'general_information' => 'General information',
+    'created_at' => 'Created at',
+    'updated_at' => 'Updated at',
+    'deleted_at' => 'Deleted at',
+    'actions' => 'Actions',
+
+    'sessions' => 'Sessions',
+    'contractType' => 'Contract Type',
+    'user' => 'User',
+];
---
+++ resources/lang/es/common.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+    'yes' => 'Si',
+    'no' => 'No',
+
+    'users' => 'Usuarios',
+    'new_user' => 'Nuevo Usuario',
+    'home' => 'Home',
+
+    'id' => 'ID',
+
+    'name' => 'Nombre',
+    'email' => 'Email',
+    'email_verified_at' => 'Email Verificado el',
+    'last_login' => 'Último Login',
+
+    'general_information' => 'Información general',
+    'created_at' => 'Creado el',
+    'updated_at' => 'Actualizado el',
+    'deleted_at' => 'Eliminado el',
+    'actions' => 'Acciones',
+
+    'sessions' => 'Sesiones',
+    'contractType' => 'Tipo de Contrato',
+    'user' => 'Usuario',
+];
